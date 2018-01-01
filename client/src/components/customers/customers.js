9e6caff1c761b30c5d8eb8bca0c3280240a18b3a
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-01 16:50:14 +0200

    Added the fetching of the customers in the customers-component

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            customers: []
+        }
+    }
+
+    componentDidMount() {
+
+        //  Fetch the data from "/client/package.json -> proxy" /api/customers
+        fetch('/api/customers')
+
+            //  convert result to json
+            .then(res => res.json())
+
+            //  Set the customers-array of the state to match the fetched result in json form.
+            .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
+    }
+
+    render() {
+        return (
+        <div>
+            <h2>Customers</h2>
+        </div>
+        );
+    }
+}
+
+export default Customers;
